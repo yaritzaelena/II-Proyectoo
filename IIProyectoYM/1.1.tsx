d82cdff66601d65fc815ec13fc05eff7fd14922a
maria1888 <48070915+maria1888@users.noreply.github.com>
2019-05-16 22:13:38 -0600

    Add files via upload

---
+++ IIProyectoYM/1.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="1" tilewidth="25" tileheight="25" tilecount="100" columns="10">
+ <image source="1.png" trans="000000" width="250" height="250"/>
+</tileset>
